30ec8370ee3421c9abed06cbd271a94e383e3fa5
Austin Warrick <austinwarrick@gmail.com>
2023-05-22 09:40:04 -0500

    added keys to settings.js

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,6 +1,6 @@
 export default {
-	graphhopperKey: "",
-	npsKey: "",
-	weatherKey: ""
+	graphhopperKey: "ebdedf4a-5be5-4ff0-b2ab-54fa6e624a4f",
+	npsKey: "SB14CPSabSBvnka022NtJOwYUqNocKf5ghvFyYhI",
+	weatherKey: "2c2f04ef34a12993464bf4dd33ee0fe8"
 }
 
