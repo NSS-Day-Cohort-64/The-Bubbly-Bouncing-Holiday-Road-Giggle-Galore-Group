9ccf171f48e058352f849d594e3a73d89181d26e
Cassie Revell <classierev@gmail.com>
2023-05-22 11:41:48 -0500

    Eateries Done

--- scripts/eateries/EateryProvider.js
+++ scripts/eateries/EateryProvider.js
@@ -1,6 +1,28 @@
+import { setEateriesId } from "./TransientState.js"
+
+const handleEatsChoice = (event) => {
+    if (event.target.id === "eateriesSelect") {
+        setEateriesId(parseInt(event.target.value))
+    }
+}
+
 export const eateryList = async () => {
-    const fetchResponse = await fetch(
+   const fetchResponse = await fetch(
         "http://holidayroad.nss.team/eateries"
     )
     const eats = await fetchResponse.json()
+
+    document.addEventListener("change", handleEatsChoice)
+
+   let optionsHTML = eats.map((eat) => {
+    return `<option value="${eat.id}">${eat.businessName}</option>`;
+  });
+
+  let dropdownHTML = `<h1>Eatery Selection</h1>
+                        <select id="eateriesSelect">
+                          <option>Eatery Choices</option>
+                          ${optionsHTML.join("")}
+                        </select>`;
+                        
+  return dropdownHTML;
 }
