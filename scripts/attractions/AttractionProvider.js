c6d6a2ab6dfe93c5b312a703e01bcaef9c3cf467
Logan Belews <Loganbelew2@gmail.com>
2023-05-23 13:29:22 -0500

    idk

--- scripts/attractions/AttractionProvider.js
+++ scripts/attractions/AttractionProvider.js
@@ -1,18 +1,12 @@
 import { setAttractionId } from "../TransientState.js"
-
-const handleAttractionChoice = (event) => {
-    if (event.target.id === "attractSelect") {
-        setAttractionId(parseInt(event.target.value))
-    }
-}
-
+let attractions;
 export const attractionList = async () => {
-     const fetchResponse = await fetch(
-        "http://holidayroad.nss.team/bizarreries"
-    )
-    const attractions = await fetchResponse.json()
+  const fetchResponse = await fetch(
+    "http://holidayroad.nss.team/bizarreries"
+  )
+  attractions = await fetchResponse.json()
 
-    document.addEventListener("change", handleAttractionChoice)
+  document.addEventListener("change", handleAttractionChoice)
 
   let optionsHTML = attractions.map((attraction) => {
     return `<option value="${attraction.id}">${attraction.name}</option>`;
@@ -25,3 +19,68 @@ export const attractionList = async () => {
 
   return dropdownHTML;
 }
+
+
+const handleAttractionChoice = (event) => {
+  if (event.target.id === "attractSelect") {
+    setAttractionId(parseInt(event.target.value))
+    const selectedAttraction = event.target.options[event.target.selectedIndex].text;
+    const selectedAttractionId = event.target.value
+    displaySelectedAttraction(selectedAttraction, selectedAttractionId);
+  }
+}
+
+
+
+
+///////////////
+
+const displaySelectedAttraction = (attractionName, attractionId) => {
+  const attractionNameContainer = document.querySelector("#attractionNameContainer");
+  if (attractionNameContainer) {
+    attractionNameContainer.innerHTML = `
+      <p>${attractionName}</p>
+      <button class="details-button" data-attraction-id="${attractionId}">Details</button>
+      <div class="details-container" id="attractionDetailsContainer"></div>
+    `;
+
+    const detailsButton = attractionNameContainer.querySelector(".details-button");
+    detailsButton.addEventListener("click", async () => {
+      await showattractionDetails(attractionId);
+    });
+  }
+};
+
+
+
+const showattractionDetails = async (attractionId) => {
+  const attractionDetailsContainer = document.querySelector("#attractionDetailsContainer");
+  if (attractionDetailsContainer) {
+    attractionDetailsContainer.innerHTML = ""; // Clear the container
+
+    try {
+      // Fetch attraction details using the attractionId
+      const response = await fetch(`http://holidayroad.nss.team/bizarreries/${attractionId}`);
+      const attractionDetails = await response.json();
+
+      // Create an unordered list with attraction details
+      const detailsList = document.createElement("ul");
+
+      // Specify the desired keys to display
+      const keysToDisplay = ["description", "state", "city"];
+
+      // Iterate through the desired keys and create list items
+      keysToDisplay.forEach((key) => {
+        if (attractionDetails.hasOwnProperty(key)) {
+          const listItemHTML = `<li><strong>${key.toUpperCase()}:</strong> ${attractionDetails[key]}</li>`;
+          detailsList.innerHTML += listItemHTML;
+        }
+      });
+
+      // Append the details list to the container
+      attractionDetailsContainer.appendChild(detailsList);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -6,7 +6,7 @@ export const parksList = async () => {
     const parks = await fetchResponse.json()
     const parksDataObj = parks.data
     let html = ""
-    html += `<select id ="parks">`
+    html += `<select id ="ParkSelect">`
     html += `<option value= "0"> Select a National Park </option>`
 
     for (const park of parksDataObj) {
@@ -16,44 +16,72 @@ export const parksList = async () => {
     return html
 }
 
-document.addEventListener(
-    "change",
-    (changeEvent) => {
-        if (changeEvent.target.id === "parks" ) {
-            setParksId(changeEvent.target.value)
-        }
+
+
+const handleParkChoice = (event) => {
+    if (event.target.id === "ParkSelect") {
+        setParksId(event.target.value)
+        const selectedPark = event.target.options[event.target.selectedIndex].text;
+        const selectedParkId = event.target.value
+        displaySelectedPark(selectedPark, selectedParkId);
     }
-)
-
-
-
-
-/*import { setPaint } from "./transient.js"
-
-export const getPaint = async () => {
-    const response = await fetch("http://localhost:8088/paints")
-    const paints = await response.json()
-
-    let html = "<h2>paint</h2>"
-
-    html += '<select id="paint">'
-    html += '<option value="0">Select a paint</option>'
-
-    const arrayOfOptions = paints.map( (paint) => {
-            return `<option value="${paint.id}">${paint.color}</option>`
-        }
-    )
-
-    html += arrayOfOptions.join("")
-    html += "</select>"
-    return html
 }
 
 
+document.addEventListener("change",handleParkChoice)
+  
+  
+  ///////////////
+  
+  const displaySelectedPark = (ParkName, ParkId) => {
+    const ParkNameContainer = document.querySelector("#ParkNameContainer");
+    if (ParkNameContainer) {
+      ParkNameContainer.innerHTML = `
+        <p>${ParkName}</p>
+        <button class="details-button" data-Park-id="${ParkId}">Details</button>
+        <div class="details-container" id="ParkDetailsContainer"></div>
+      `;
+  
+      const detailsButton = ParkNameContainer.querySelector(".details-button");
+      detailsButton.addEventListener("click", async () => {
+        await showParkDetails(ParkId);
+      });
+    }
+  };
+  
+  
+  
+  const showParkDetails = async (ParkId) => {
+    const ParkDetailsContainer = document.querySelector("#ParkDetailsContainer");
+    if (ParkDetailsContainer) {
+      ParkDetailsContainer.innerHTML = ""; // Clear the container
+  
+      try {
+        // Fetch Park details using the ParkId
+        const response = await fetch(`https://developer.nps.gov/api/v1/parks${ParkId}?api_key=SB14CPSabSBvnka022NtJOwYUqNocKf5ghvFyYhI`);
+        const ParkDetails = await response.json();
+        const dataArr = ParkDetails.data
+       const myPark = dataArr[ParkId]
+  
+        // Create an unordered list with Park details
+        const detailsList = document.createElement("ul");
+  
+        // Specify the desired keys to display
+        const keysToDisplay = ["description", "fullName", "parkCode"];
+  
+        // Iterate through the desired keys and create list items
+        keysToDisplay.forEach((key) => {
+          if (myPark.hasOwnProperty(key)) {
+            const listItemHTML = `<li><strong>${key.toUpperCase()}:</strong> ${myPark[key]}</li>`;
+            detailsList.innerHTML += listItemHTML;
+          }
+        });
+  
+        // Append the details list to the container
+        ParkDetailsContainer.appendChild(detailsList);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-const paintHandler = function (event) {
-    if(event.target.id === 'paint' )
-    setPaint(event.target.value)
-}
-
-document.addEventListener("change", paintHandler)*/
