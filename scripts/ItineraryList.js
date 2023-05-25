ac294270fc981f515dad160fd9f08eec5dc353f8
Logan Belews <Loganbelew2@gmail.com>
2023-05-25 09:41:45 -0500

    commit

--- scripts/ItineraryList.js
+++ scripts/ItineraryList.js
@@ -1,28 +1,30 @@
-
-
+let parkFullName;
+let itineraries;
+let eateryBusinessName;
+let attractionName;
 export const savedItineraries = async () => {
     const fetchResponse = await fetch("http://localhost:8088/itineraries");
-    const itineraries = await fetchResponse.json();
+     itineraries = await fetchResponse.json();
     let ordersHTML = "";
-  
+    document.addEventListener("click", handleDirections)
     for (const itinerary of itineraries) {
       const { attractionsId, eateriesId, parksId } = itinerary;
   
       // Fetch attraction details
       const attractionResponse = await fetch(`http://holidayroad.nss.team/bizarreries/${attractionsId}`);
       const attractionData = await attractionResponse.json();
-      const attractionName = attractionData.name;
+       attractionName = attractionData.name;
   
       // Fetch eatery details
       const eateryResponse = await fetch(`http://holidayroad.nss.team/eateries/${eateriesId}`);
       const eateryData = await eateryResponse.json();
-      const eateryBusinessName = eateryData.businessName;
+       eateryBusinessName = eateryData.businessName;
   
       // Fetch park details
       const parksResponse = await fetch("https://developer.nps.gov/api/v1/parks?api_key=SB14CPSabSBvnka022NtJOwYUqNocKf5ghvFyYhI");
       const parksData = await parksResponse.json();
       const park = parksData.data.find((park) => park.id === parksId);
-      const parkFullName = park.fullName;
+       parkFullName = park ? park.fullName : "Unknown Park";
   
       const orderElement = document.createElement("div");
       orderElement.classList.add("itineraryList");
@@ -32,7 +34,7 @@ export const savedItineraries = async () => {
         <li>Eatery: ${eateryBusinessName}</li>
         <li>Attraction: ${attractionName}</li>
       </ul>
-      <div> <button id = "directButton"> Get Directions</button>
+      <div> <button data id = "directButton"> Get Directions</button>
       `;
   
       ordersHTML += orderElement.outerHTML;
@@ -40,13 +42,28 @@ export const savedItineraries = async () => {
   
     return ordersHTML;
   };
-
-  const directionButtons = () => {
-    document.addEventListener("click", handleDirections)
-  }
   
   const handleDirections = (clickEvent) => {
-    if (clickEvent.id === 'directButton') {
-      
+    if (clickEvent.target.id === 'directButton') {
+      getParkDirections()
     }
   }
+
+  const getParkDirections = async() => {
+    const response = await fetch(`https://graphhopper.com/api/1/geocode?q=${parkFullName}&locale=us&debug=true&key=18ddaec7-41f3-4433-91c3-33acd1099232`)
+    const geocode = await response.json()
+    const parkLat = geocode.hits
+    const parkLng = geocode.hits
+
+    const responsee = await fetch(`https://graphhopper.com/api/1/geocode?q=${eateryBusinessName}&locale=us&debug=true&key=18ddaec7-41f3-4433-91c3-33acd1099232`)
+    const geocodee = await responsee.json()
+    
+    const responseee = await fetch(`https://graphhopper.com/api/1/geocode?q=${attractionName}&locale=us&debug=true&key=18ddaec7-41f3-4433-91c3-33acd1099232`)
+    const geocodeee = await responseee.json()
+
+
+  }
+
+ 
+  
+
--- scripts/eateries/EateryProvider.js
+++ scripts/eateries/EateryProvider.js
@@ -1,5 +1,7 @@
 import { setEateriesId } from "../TransientState.js";
-
+// function sets var to element in main using query then checks if it exsists before setting the innerhtml of the element.
+// sets empty div then selects details button and add event listener of click to it then invokes showEateryDetails func with
+// argument of a param
 const displaySelectedEatery = (eateryName, eateryId) => {
   const eateryNameContainer = document.querySelector("#eateryNameContainer");
   if (eateryNameContainer) {
@@ -15,7 +17,9 @@ const displaySelectedEatery = (eateryName, eateryId) => {
     });
   }
 };
-
+//function selects element in dom, fetches the eatery that was selected then creates ul and var that is a array of strings/keys
+// uses forEach method on the array of keys then checks if the selected eatery hasownproperty of each key, sets var to html
+// list items that have the desired keys and appends the ul with list items then appends the list to eaterydetails
 const showEateryDetails = async (eateryId) => {
   const eateryDetailsContainer = document.querySelector("#eateryDetailsContainer");
   if (eateryDetailsContainer) {
@@ -47,17 +51,19 @@ const showEateryDetails = async (eateryId) => {
     }
   }
 };
-
+// function checks if change events id is eatSelect, sets the event's value attribute to var, invokes setter function with var
+// sets the selected option's text to var and invokes displayEatery function with arguments of the variables
 const handleEatChoice = (event) => {
   if (event.target.id === "eatSelect") {
     const selectedEateryId = event.target.value;
-    setEateriesId(parseInt(event.target.value));
+    setEateriesId(parseInt(selectedEateryId));
 
     const selectedEatery = event.target.options[event.target.selectedIndex].text;
     displaySelectedEatery(selectedEatery, selectedEateryId);
   }
 };
-
+//function fetches eateries data, adds event listener for change event and passes callback function, maps of each eatery and puts
+// the name as a opton in a dropdown via join method then returns dropdown
 export const eateryList = async () => {
   const fetchResponse = await fetch("http://holidayroad.nss.team/eateries");
   const eats = await fetchResponse.json();
